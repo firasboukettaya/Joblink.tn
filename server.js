9c0de219da813a3302cc4eed38686c39cd148a42
JobLink Tunisia <tn.joblink@gmail.com>
2025-11-28 10:43:14 -0500

    Add homepage with API documentation

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const db = new Database(process.env.DATABASE_URL || './joblink.db');
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // Initialiser la base de données
 function initDatabase() {
